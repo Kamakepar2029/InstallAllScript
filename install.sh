52862ccaf562abc3fcdfe7cf19157942c7974d92
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-05-04 10:54:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sudo apt install -y tightvncserver && echo "[+] Vnc Installed" || echo "[x] Vnc Not Installed"
+sudo apt-get install xfonts-base -y && echo "[+] Addons Installed" || echo "[x] Addons not installed :-("
+sudo apt-get install firefox -y && echo "[+] Browser Installed Successfully" || echo "[x] Browser not installed"
 echo "[!] Now you need to set password for vncserver"
 tightvncserver
 echo "[!] Starting installation VNC Service"
@@ -17,6 +19,5 @@ echo "[Install]">>$fii
 echo "WantedBy=multi-user.target">>$fii
 echo "[*] Setting user Permissions"
 sudo chmod a=rwx /etc/systemd/system/tightvncserver.service && echo "[+] User Permission Successfully Set" || echo "[-] User Permissions were set Not Successfully"
-sudo systemctl start tightvncserver.service && echo "[+] Service started" || echo "[-] Service not started"
 sudo systemctl enable tightvncserver.service && echo "[+] Service enabled" || echo "[-] Service not enabled"
 echo "[Install Success ;-)]"
